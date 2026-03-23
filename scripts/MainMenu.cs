20a4ad92a253e1509e9bbbf807417f1659aee7d3
Drew Taylor <drew@taylorspot.com>
2026-03-23 18:15:31 -0400

    Some visual edits

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -4,27 +4,27 @@ namespace ARPG;
 
 public partial class MainMenu : Control
 {
-    public override void _Ready()
-    {
-        Input.MouseMode = Input.MouseModeEnum.Visible;
-        GetNode<ColorRect>("Background").Color = Palette.BgDark;
+	public override void _Ready()
+	{
+		Input.MouseMode = Input.MouseModeEnum.Visible;
+		GetNode<ColorRect>("Background").Color = Palette.BgDark;
 
-        var playButton = GetNode<Button>("Background/GridContainer/PlayButton");
-        Palette.StyleButton(playButton, 28);
-        playButton.Pressed += OnPlayPressed;
+		var playButton = GetNode<Button>("Background/GridContainer2/GridContainer/PlayButton");
+		Palette.StyleButton(playButton, 28);
+		playButton.Pressed += OnPlayPressed;
 
-        var quitButton = GetNode<Button>("Background/GridContainer/QuitButton");
-        Palette.StyleButton(quitButton, 28);
-        quitButton.Pressed += OnQuitPressed;
-    }
+		var quitButton = GetNode<Button>("Background/GridContainer2/GridContainer/QuitButton");
+		Palette.StyleButton(quitButton, 28);
+		quitButton.Pressed += OnQuitPressed;
+	}
 
-    private void OnPlayPressed()
-    {
-        GetTree().ChangeSceneToFile("res://scenes/ArchetypeSelect.tscn");
-    }
+	private void OnPlayPressed()
+	{
+		GetTree().ChangeSceneToFile("res://scenes/ArchetypeSelect.tscn");
+	}
 
-    private void OnQuitPressed()
-    {
-        GetTree().Quit();
-    }
+	private void OnQuitPressed()
+	{
+		GetTree().Quit();
+	}
 }
